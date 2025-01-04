2d204b575fcab7c97ad83bba1972b9e0fbdc6d7d
rengoku2001 <venkateshjatavath2@gmail.com>
2025-01-04 17:56:06 +0530

    Update test.sh

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,3 +1,6 @@
-for j in {1..10}; do  for i in {1..1000}; do
-    curl -s -o /dev/null -w "%{http_code}\n" http://a0fbf831ca4a94936954790ef7a89bc4-104519041.ap-south-1.elb.amazonaws.com/ &  done
-  waitdone
+for j in {1..10}; do
+  for i in {1..1000}; do
+    curl -s -o /dev/null -w "%{http_code}\n" http://a04c1e0587102451cb84237b0d2de8bd-227543275.ap-south-1.elb.amazonaws.com/ &
+  done
+  wait
+done
